5d1739f139f52eeba30e56f8dabb6fc65eb4f59a
Nathan Klingensmith <njklingensmith@wpi.edu>
2020-10-24 17:24:37 -0400

    Add routing, add nav buttons, add material module, create home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
-import { NavBarComponent } from './global-components/nav-bar/nav-bar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppComponent } from './app.component';
+import { NgModule } from '@angular/core';
+import { MaterialModule } from './material.module';
+
+import { RouterModule, Routes } from '@angular/router';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import { NavBarComponent } from './global-components/nav-bar/nav-bar.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+
+const routes: Routes = [
+  { path: 'home', component: HomePageComponent },
+  { path: 'development', component: HomePageComponent },
+  { path: 'design', component: HomePageComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full'}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    HomePageComponent
   ],
   imports: [
+    MaterialModule,
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes),
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
