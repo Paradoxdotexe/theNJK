8b39a5fd36c0f18442e959a4a975e8bc8e3c79a8
Nathan Klingensmith <njklingensmith@wpi.edu>
2020-12-18 13:54:09 -0500

    Add image placeholders while loading

--- src/app/pages/design-page/design-page.component.ts
+++ src/app/pages/design-page/design-page.component.ts
@@ -14,9 +14,11 @@ export class DesignPageComponent implements OnInit {
   entryRows = DesignEntryRows;
   entries = DesignEntries;
   entry = null; // viewed entry
+  entryOpened = false; // if entry is fully open
 
   contentPath;  // path to entry content based on contentKey
   contentIndices; // [0, 1, 2, ...contentCount], used to generate list of entry images
+  loaded = {}; // dictionary of whether content images have loaded
 
   constructor() { }
 
@@ -28,15 +30,29 @@ export class DesignPageComponent implements OnInit {
     this.contentPath = 'assets/content/design/' + this.entry.contentKey + '_';
     this.contentIndices = Array(this.entry.contentCount).fill(0).map((x, i) => i + 1);
 
-    this.sidenav.open().then();
+    this.sidenav.open().then(() => this.entryOpened = true);
     document.body.classList.add('prevent-scrolling');
     this.overlay.nativeElement.classList.add('show');
+    this.overlay.nativeElement.classList.add('block');
   }
 
   closeEntry(): void {
-    this.sidenav.close().then();
-    document.body.classList.remove('prevent-scrolling');
-    this.overlay.nativeElement.classList.remove('show');
+    if (this.entryOpened) {
+      this.sidenav.close().then(() => {
+        this.entry = null;
+        this.entryOpened = false;
+      });
+      document.body.classList.remove('prevent-scrolling');
+      this.overlay.nativeElement.classList.remove('show');
+      this.delay(600).then(() => {
+        this.overlay.nativeElement.classList.remove('block');
+      });
+    }
+  }
+
+  // asynchronous time delay
+  async delay(ms: number): Promise<null> {
+    return new Promise( resolve => setTimeout(resolve, ms) );
   }
 
 }
